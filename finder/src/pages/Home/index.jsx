b963de1741ff5666a6cbfa1df87092b80dea2bf3
narradorww <rodrigo.anst@gmail.com>
2021-10-20 09:35:20 -0300

    Organizado o grid,  instalada a fonte

--- finder/src/pages/Home/index.jsx
+++ finder/src/pages/Home/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import {Container} from './style'
+import React, {useState} from 'react'
+import {Container, Logo, Map, Search, Wrapper} from './style'
+import TextField, {Input} from '@material/react-text-field'
+import logo from '../../assets/logo.svg'
+import MaterialIcon from '@material/react-material-icon'
 
-const Home =()=>
+
+const Home =()=> {
+    const [inputValue, setInputValue] = useState('')
+    
+    return(
+    
+    
+    <Wrapper>
     <Container>
-        Hello, world!
+        <Search><Logo src={logo} alt= "logo do buscador"></Logo>
+        <TextField
+          label='Pesquisar'
+          outlined
+          //onTrailingIconSelect={() => this.setState({value: ''})}
+         trailingIcon={<MaterialIcon role="button" icon="location_searching"/>}
+        ><Input
+           value={inputValue}
+           onChange={(e) => setInputValue(e.target.value)} />
+        </TextField>
+        
+        
+        </Search>
     </Container>
+    <Map/>
+    </Wrapper>
+    )
+    
+};
 
 
 export default Home;
--- finder/src/pages/Home/style.js
+++ finder/src/pages/Home/style.js
@@ -1,8 +1,36 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-    //background-color: ${(props)=> props.theme.colors.primary};
 
-
+export const Wrapper = styled.div`
+display: flex;
+ flex-direction: row
 `;
 
+export const Container = styled.aside`
+    background-color: ${(props)=> props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
+`;
+
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color:#fff;
+    padding: 16px;
+
+    
+`;
+
+
+export const Logo = styled.img`
+    margin-bottom: 15px;
+`;
+
+
+export const Map = styled.div`
+    background-color: orangered ;
+    width: 100%;
+`;
